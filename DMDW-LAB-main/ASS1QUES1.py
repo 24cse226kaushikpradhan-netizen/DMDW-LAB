4fe707bc31526d561dc9ae387417f3ddf8d77167
24cse226kaushikpradhan-netizen <24cse226.kaushikpradhan@giet.edu>
2026-07-21 17:13:14 +0530

    Delete DMDW-LAB-main directory

--- DMDW-LAB-main/ASS1QUES1.py
+++
@@ -1,4 +0,0 @@
-#WAP to input a string and display its reverse string
-str = input("Enter a string: ")
-print("Actual String:", str)
-print("Reversed String:", str[::-1])
--- DMDW-LAB-main/ASS1QUES4.py
+++
@@ -1,7 +0,0 @@
-#WAP to create a set with some sample elements and then display them.
-
-s1 = {1, 2, 3}
-print(s1)
-print(type(s1))
-for i in s1:
-    print(i)
